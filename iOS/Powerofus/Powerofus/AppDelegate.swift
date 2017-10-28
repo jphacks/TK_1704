d163665ea07013fc6c93a44f36aac12d9180af2f
nakarin0528 <n1510121@edu.cc.uec.ac.jp>
2017-10-28 22:09:11 +0900

    training用のVCを作った

--- iOS/Powerofus/Powerofus/AppDelegate.swift
+++ iOS/Powerofus/Powerofus/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
         window!.backgroundColor = UIColor.white
-        window!.rootViewController = TopVC()
+        window!.rootViewController = TrainingVC()
         window!.makeKeyAndVisible()
         
         return true
