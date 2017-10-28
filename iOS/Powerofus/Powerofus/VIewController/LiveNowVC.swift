ff4873b58a1bf1a950d3f56cbbe037048c2f929c
nakarin0528 <n1510121@edu.cc.uec.ac.jp>
2017-10-29 07:05:07 +0900

    色を追加

--- iOS/Powerofus/Powerofus/VIewController/LiveNowVC.swift
+++ iOS/Powerofus/Powerofus/VIewController/LiveNowVC.swift
@@ -1,7 +1,7 @@
 import UIKit
 import SnapKit
 import Material
-
+import AudioToolbox
 
 class LiveNowVC: UIViewController {
     
@@ -24,6 +24,26 @@ class LiveNowVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        switch socketModel.color {
+        case "red":
+            navigationController?.navigationBar.barTintColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+            self.view.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        case "yellow":
+            navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)
+            self.view.backgroundColor = #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)
+        case "pink":
+            navigationController?.navigationBar.barTintColor = #colorLiteral(red: 1, green: 0.5409764051, blue: 0.8473142982, alpha: 1)
+            self.view.backgroundColor = #colorLiteral(red: 1, green: 0.5409764051, blue: 0.8473142982, alpha: 1)
+        case "green":
+            navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
+            self.view.backgroundColor = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
+        case "purple":
+            navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.5818830132, green: 0.2156915367, blue: 1, alpha: 1)
+            self.view.backgroundColor = #colorLiteral(red: 0.5818830132, green: 0.2156915367, blue: 1, alpha: 1)
+        default:
+            break
+        }
 
         motionModel.startAccelerometer()
         
