2958e720b37d91c895d08b6835627dcaef656b6c
shooontan <logo.bee.doo19@gmail.com>
2017-10-29 11:10:29 +0900

    にょきにょき

--- server/routes/socketIO.js
+++ server/routes/socketIO.js
@@ -13,10 +13,10 @@ db.action = new Datastore({
   filename: path.join(__dirname, '../NeDB/action'),
   autoload: true
 });
-db.live = new Datastore({
-  filename: path.join(__dirname, '../NeDB/live'),
-  autoload: true
-});
+// db.live = new Datastore({
+//   filename: path.join(__dirname, '../NeDB/live'),
+//   autoload: true
+// });
 
 
 //appモジュールを読み込む（このappには、www内ですでにポート番号が登録されているので、ポート番号を取得したことにもなる）
@@ -31,6 +31,16 @@ const server = http.createServer(app);
 //サーバーをlistenしてsocketIOを設定
 const io = require('socket.io')(server);
 
+// 乱数を取得
+const getRandom = (len = 16) => {
+  var str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < len; i++) {
+    result += str.charAt(Math.floor(Math.random() * str.length));
+  }
+  return result;
+}
+
 //socketIOモジュール
 function socketIO() {
   //サーバーを立ち上げたら実行
@@ -39,8 +49,8 @@ function socketIO() {
   });
 
   // ライブid
-  let liveId = 'adfijre';
-
+  let liveId = 'thisisliveid';
+  let living = false;
 
   // socket の接続
   io.on('connection', (socket) => {
@@ -49,8 +59,6 @@ function socketIO() {
 
     // 色ごとの部屋に別ける
     socket.on('join_room', (msg) => {
-      // console.log(msg);
-
       // ユーザーデータをDBに登録
       db.user.insert({
         color: msg.user.color,
@@ -78,6 +86,10 @@ function socketIO() {
 
     // アプリからサーバーへのアクションデータを取得
     socket.on('server_from_app', (msg) => {
+      if (living !== true) {
+        return;
+      }
+
       const { all, duration } = msg.score;
       const { id, name, color } = msg.user;
 
@@ -88,7 +100,6 @@ function socketIO() {
         liveId: liveId
       })
         .then(insertDoc => {
-
           db.action.find({
             color: color,
             liveId: liveId
@@ -130,17 +141,76 @@ function socketIO() {
       io.sockets.emit('app', msg);
     });
 
+    // 音楽が開始された
+    socket.on('start_web_music', () => {
+      if (living === true) {
+        return;
+      }
+      // ライブのidをランダムで設定
+      liveId = getRandom();
+      living = true;
+
+      // アプリ側に音楽が始まったことを知らせる
+      io.sockets.emit('start_music', {
+        action: {
+          type: "start"
+        }
+      });
+    });
+
+    // 音楽が終わった
+    socket.on('stop_web_music', () => {
+      if (living === false) {
+        return;
+      }
+      living = false;
+
+      // アプリ側に音楽が終わったことを知らせる
+      io.sockets.emit('stop_music', {
+        action: {
+          type: "stop"
+        }
+      });
+    });
+
     //jsonfileを読み出して返すイベント
-    socket.on('readJSON', function (color) {
-      var rawdata = fs.readFileSync( __dirname + '/../humin/id_data.json', 'utf-8');
-      var data = JSON.parse(rawdata);
-      var score = data[color];
-      var name = `json${color}`;
-      io.sockets.emit(name, { value: score });
+    socket.on('read_json', () => {
+      db.action.find({
+        liveId: liveId
+      })
+        .then(findDoc => {
+
+          const colorData = {
+            red: 0,
+            yellow: 0,
+            pink: 0,
+            green: 0,
+            purple: 0
+          };
+
+          findDoc.forEach((element) => {
+            switch (element.color) {
+              case 'red':
+              case 'yellow':
+              case 'pink':
+              case 'green':
+              case 'purple':
+                colorData[element.color] = colorData[element.color] + element.scoreDuration;
+                break;
+            }
+          });
+          console.log("all_color_sum")
+          io.sockets.connected[socket.id].emit('all_color_sum', colorData);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
     });
 
   });
 };
+
 
 //export
 module.exports = socketIO;
